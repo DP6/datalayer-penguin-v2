df918062c5bc486e9714cb2de7a039a043b65951
Lucas <lucas.alves@dp6.com.br>
2023-04-05 17:28:34 -0300

    docs: :memo: documentei o código do crawler e da validação para facilitar o entendimento

--- crawler.js
+++ crawler.js
@@ -5,7 +5,7 @@ import AdblockerPlugin from 'puppeteer-extra-plugin-adblocker';
 import StealthPlugin from 'puppeteer-extra-plugin-stealth';
   puppeteer.use(StealthPlugin());
 
-let [config_file, stopAdBlock] = process.argv.slice(2)
+let [config_file, stopAdBlock] = process.argv.slice(2) // responsible for commands entered at the prompt
 
 const config = JSON.parse( // this const import json configuration schema from config folder
       await readFileSync(
@@ -13,36 +13,37 @@ const config = JSON.parse( // this const import json configuration schema from c
       )
     );
 
-const schema = JSON.parse( // this const import json configuration schema from config folder
+const schema = JSON.parse( // this const import json schema from config folder
       await readFileSync(
         new URL(`./schema/${config.validator[0].schema_name[0]}.json`, import.meta.url)
       )
     );
 
-if(stopAdBlock !== "stopAdBlock"){
+if(stopAdBlock !== "stopAdBlock"){ // to stop adblock functioning
     puppeteer.use(AdblockerPlugin({ useCache: false }));
   }
     
-    (async () => {
+    (async () => { // opening and validation function in the browser
       const browser = await puppeteer.launch({headless: false}); // default is true;
       const page = await browser.newPage();
-      await page.goto(config.validator[0].url[0]);
-      await page.setViewport({width: 1080, height: 1024});
-      await page.exposeFunction('bowser', (elm) => {
+      await page.goto(config.validator[0].url[0]); // website to be opened
+      await page.setViewport({width: 1080, height: 1024}); // screen size
+      await page.exposeFunction('bowser', (elm) => { //intermediary function between machine and browser (injects the validation function into the browser)
             validation(schema.items,elm)
       });
-      await page.evaluate(async () => {
-        for(let elem of window.dataLayer){
-          if(elem.event !== "optimize.domChange"){
-          bowser(elem)}
+      await page.evaluate(async () => { //performs the validation process in the browser
+        for(let elem of window.dataLayer){ //get dataLayer events
+          if(elem.event !== "optimize.domChange"){ // 
+          bowser(elem)} // validate
         }
-        window.dataLayer.push_c = window.dataLayer.push;
+        window.dataLayer.push_c = window.dataLayer.push; //change event push
+
         window.dataLayer.push = function (obj) { 
           if(obj.event !== "optimize.domChange"){
             window.dataLayer.push_c(obj);
-            bowser(obj)}}
+            bowser(obj)}} // validates the new dataLayer push
         })
-        if (config.validator[0].browserClose) {
+        if (config.validator[0].browserClose) { // check the time and close the browser
           config.validator[0].time ? await page.waitForTimeout(config.validator[0].time) : await page.waitForTimeout(0);
           await browser.close();
         }
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import Ajv from 'ajv'
 import { AggregateAjvError } from '@segment/ajv-human-errors'
-import { isArray } from 'util'
+
 
 const ajv = new Ajv({
   allErrors: true,
@@ -8,7 +8,7 @@ const ajv = new Ajv({
   strict:false
 })
 
-function takeKeysOfSchema(schema){
+function takeKeysOfSchema(schema){ // Function responsible for retrieving the sub-property of the event if there is more than one event with the same name in the schema.
   
   for(let [key] of Object.entries(schema.properties))
   if(key !== 'event'){
@@ -16,14 +16,14 @@ function takeKeysOfSchema(schema){
   }
 }
 
-function takeKeysOfDataLayer(data){
+function takeKeysOfDataLayer(data){ // Function responsible for retrieving the sub-property of the event if there is more than one event with the same name in the dataLayer.
   for(let [key] of Object.entries(data))
   if(key !== 'event'){
     return key
   }
 }
 let arr = []
-function takeEventNameOfDataLayer(data){
+function takeEventNameOfDataLayer(data){ //Function responsible for checking if there is more than one event with the same name in dataLayer.
 for(let [key,value] of Object.entries(data)){
   if(key === 'event'){
     arr.push(value)
@@ -39,27 +39,27 @@ for(let [key,value] of Object.entries(data)){
 }
 
 
-function validation(schema, data) {
-  for (let i = 0; i < schema.length; i++) {
-    if (schema[i].properties.event.enum[0] === data.event) {
-      if(takeEventNameOfDataLayer(data) === true){
-        if(takeKeysOfSchema(schema[i]) === takeKeysOfDataLayer(data)){
-            const valid = ajv.validate(schema[i], data)
-            if (!valid){
-              const errors = new AggregateAjvError(ajv.errors)
+function validation(schema, data) { // validation function
+  for (let i = 0; i < schema.length; i++) { // check schema size
+    if (schema[i].properties.event.enum[0] === data.event) { // checks if event name in schema is the same in dataLayer
+      if(takeEventNameOfDataLayer(data) === true){ // checks if the event name is repeated in the dataLayer
+        if(takeKeysOfSchema(schema[i]) === takeKeysOfDataLayer(data)){ // if the name is repeated it takes the corresponding event in the schema and does the validation process
+            const valid = ajv.validate(schema[i], data) // ajv validation
+            if (!valid){ // if ajv catches an error
+              const errors = new AggregateAjvError(ajv.errors) // makes the error message more human
               let arr = []
               arr.push(errors)
               if(!!arr[0].message){
-              console.log(`${arr[0].message} in the ${JSON.stringify(data,null,2)}`)
+              console.log(`${arr[0].message} in the ${JSON.stringify(data,null,2)}`) // error mesage
               }
-              schema.split(i,1)
+              schema.split(i,1) // deletes the validated event from the schema 
             }else{
-              console.log(`correct in ${JSON.stringify(data,null,2)}`)
-              schema.split(i,1)
+              console.log(`correct in ${JSON.stringify(data,null,2)}`)  // if the dataLayer is correct
+              schema.split(i,1) // deletes the validated event from the schema
             }      
          }
-      }else{
-        const valid = ajv.validate(schema[i], data)
+      }else{ // if there is no event with the same name in the dataLayer
+        const valid = ajv.validate(schema[i], data) 
         if (!valid){
           const errors = new AggregateAjvError(ajv.errors)
           let arr = []
@@ -75,8 +75,8 @@ function validation(schema, data) {
       }
     }
   }
-  return console.log(`The dataLayer event is not present in the schema ${JSON.stringify(data,null,2)}`)
+  return console.log(`The dataLayer event is not present in the schema ${JSON.stringify(data,null,2)}`) //if the dataLayer event is not in the schema
+
 }
 
 export default(validation)
-
